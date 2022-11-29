65e46a64e87c3cc9aa2ede109bcd89a60ff411cf
Johnnathan Gallego <jonatanag.915@outlook.com>
2022-11-29 08:42:50 -0500

    inicio de pruebas en Javascript

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+console.log("archivo enlazado")
+let nombreusuario = prompt('Nombre de Usuario');
+console.log(nombreusuario);
+
+let edadusuario = prompt('Edad de Usuario');
+console.log(edadusuario);
+
+let nombreyedad =nombreusuario + " " + edadusuario;
+document.write(nombreyedad);
