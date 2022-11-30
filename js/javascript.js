7b5a0e6e5ccb69520b576c377de8bcedb0a88535
johnnathan Gallego <jonatan.915@outlook.com>
2022-11-29 22:03:33 -0500

    avance Javascript

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,4 @@
+/*
 console.log("archivo enlazado")
 let nombreusuario = prompt('Nombre de Usuario');
 console.log(nombreusuario);
@@ -7,3 +8,23 @@ console.log(edadusuario);
 
 let nombreyedad =nombreusuario + " " + edadusuario;
 document.write(nombreyedad);
+*/
+
+let numero1= parseInt(prompt('Ingrese el primer numero'));
+let numero2= parseInt(prompt('Ingrese el Segundo numero'));
+
+
+let resultado= numero1+numero2;
+document.write("el resultado de la suma es:", numero1,"+ ",numero2,"=", resultado);
+document.write('<br>')
+
+resultado= numero1 - numero2
+document.write("el resultado de la Resta es:", numero1,"+ ",numero2,"=", resultado);
+document.write('<br>')
+
+resultado= numero1 * numero2
+document.write("el resultado de la Multiplicación es:", numero1,"+ ",numero2,"=", resultado);
+document.write('<br>')
+resultado= numero1 / numero2
+document.write("el resultado de la División es:", numero1,"+ ",numero2,"=", resultado);
+document.write('<br>')
